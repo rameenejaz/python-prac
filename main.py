f56b25f8120a2cfdbed94fc681abf47a31e002de
Rameen Ejaz <rameenejaz4@gmail.com>
2026-02-17 23:42:13 +0500

    lab 1 tasks

--- main.py
+++ main.py
@@ -51,14 +51,14 @@
 # grade(mars);    
 
 #lab 1, lab task 5
-# number=int(input("enter a number: "));
-# factorial=1;
-# if number<0:
-#     print("factorial does not exist for negative numbers");
-# elif number==0:
-#     print("factorial of 0 is 1");       
-# else:   
-#     for i in range(1,number+1):
-#         factorial*=i;
-#     print("factorial of ",number," is ",factorial);
+number=int(input("enter a number: "));
+factorial=1;
+if number<0:
+    print("factorial does not exist for negative numbers");
+elif number==0:
+    print("factorial of 0 is 1");       
+else:   
+    for i in range(1,number+1):
+        factorial*=i;
+    print("factorial of ",number," is ",factorial);
 
