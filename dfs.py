043df9551ec2eafadd674c157202f5ef3981affa
Rameen Ejaz <rameenejaz4@gmail.com>
2026-02-28 13:03:49 +0500

    dfs traversal

--- dfs.py
+++ dfs.py
@@ -6,3 +6,23 @@ graph={
     '5':[],
     '6':[]
 }
+def dfs(graph, start_node, end_node):
+    visited=set();
+    if start_node not in graph:
+        print("node does not exist in the graph");
+        return;
+    stack=[start_node];
+    while stack:
+        current=stack.pop();
+        if current not in visited:
+            visited.add(current);
+            if current==end_node:
+                print("goal node found: ",current);
+                return True;
+            for i in reversed(graph[current]):
+                if i not in visited:
+                    stack.append(i);
+    print("goal node not found");
+    return False;
+dfs (graph,'1','6');
+            
